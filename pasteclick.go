3daf86de994986dc7e8dba5e8503c88d2022d5a8
Mark Agarunov <mark.agarunov@isprime.com>
2016-04-09 18:51:26 -0400

    add ssl to nginx conf, gofmt

--- pasteclick.go
+++ pasteclick.go
@@ -181,7 +181,7 @@ func savePost(post *http.Request) []byte {
 		log.Print(err)
 	}
 	log.Printf("[ %v ] New File: %v", post.Header.Get("X-Real-IP"), filePath)
-   fileUrl := strings.Join([]string{post.Header.Get("X-Scheme"),"://", post.Host, post.RequestURI, code, "\n"}, "")
+	fileUrl := strings.Join([]string{post.Header.Get("X-Scheme"), "://", post.Host, post.RequestURI, code, "\n"}, "")
 	return []byte(fileUrl)
 }
 func randSeq(n int, path string) []rune {
