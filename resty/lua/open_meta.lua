640f1d885f060661e2d9688abe4fdf8d33908128
Nate Catelli <ncatelli@packetfire.org>
2017-11-14 11:45:11 -0500

    Meta header to upper.

--- resty/lua/open_meta.lua
+++ resty/lua/open_meta.lua
@@ -1,6 +1,13 @@
 local cjson = require "cjson"
 local baseFile = ngx.var.uri:match("[^/]+$")
 
+local function FormatHeaderKey(key)
+  local fk = ""
+  fk = string.gsub(key, "^%l", string.upper)
+  fk = string.gsub(fk, "(-.)", function(a) return string.upper(a) end)
+  return fk 
+end
+
 local metaFile = io.open(ngx.var.document_root .. "/_" .. baseFile, "r")
 if metaFile == nil then 
   ngx.exec("@no_meta", "") 
@@ -15,7 +22,7 @@ local mime = meta["mime_type"]
 
 if ngx.var.lua_obj_uri then
   for k, v in pairs(meta) do
-    ngx.header['PC-' .. k] = v
+    ngx.header['PC-' .. FormatHeaderKey(k)] = v
   end
 
   ngx.exec("@serve_obj", "")
