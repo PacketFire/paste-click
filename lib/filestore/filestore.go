984c88570fbad6d2b6d7838d6cfd8c626422ef43
Nate Catelli <ncatelli@packetfire.org>
2018-12-16 20:03:51 -0500

    Fix FileStore interface to use ObjectID for read.

--- lib/filestore/filestore.go
+++ lib/filestore/filestore.go
@@ -2,8 +2,8 @@ package filestore
 
 // FileStore provides the interface for persisting files posted to the api.
 type FileStore interface {
-	Init() error         // Initializes a Filestore
-	Write(*Object) error // Attempts to write an Object to the filestore.
-	Read(string) *Object // Takes an object identifier as an argument and retrieves it from the store.
-	Close() error        // Safely shutsdown a filestore.
+	Init() error           // Initializes a Filestore
+	Write(*Object) error   // Attempts to write an Object to the filestore.
+	Read(ObjectID) *Object // Takes an object identifier as an argument and retrieves it from the store.
+	Close() error          // Safely shutsdown a filestore.
 }
