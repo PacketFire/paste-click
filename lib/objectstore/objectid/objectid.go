6b7254dff8da186c683a503ecac66eabbcebf304
Nate Catelli <ncatelli@packetfire.org>
2019-01-01 15:24:07 -0500

    Consistently generate a hash.

--- lib/objectstore/objectid/objectid.go
+++ lib/objectstore/objectid/objectid.go
@@ -1,4 +1,16 @@
 package objectid
 
+import (
+	"hash"
+)
+
 // ObjectID contains the string representation of an objects identifier.
 type ObjectID string
+
+// New takes a Hash as an argument and uses that to generate a new ObjectID.
+func New(h hash.Hash) ObjectID {
+	// Generate checksum and truncate it to 6 characters
+	checksum := string(h.Sum(nil))
+
+	return ObjectID(checksum)
+}
