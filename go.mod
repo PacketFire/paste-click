1bedd54c0dd810352da529ac042cf419be4c9536
Nate Catelli <ncatelli@packetfire.org>
2019-01-02 18:51:49 -0500

    Update to use libmagic

--- go.mod
+++ go.mod
@@ -3,4 +3,5 @@ module github.com/PacketFire/paste-click
 require (
 	github.com/caarlos0/env v3.5.0+incompatible
 	github.com/gorilla/mux v1.6.2
+	github.com/rakyll/magicmime v0.1.0
 )
--- handlers/upload/handler.go
+++ handlers/upload/handler.go
@@ -2,10 +2,12 @@ package upload
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
 
 	"github.com/PacketFire/paste-click/lib/objectstore"
+	"github.com/rakyll/magicmime"
 )
 
 // Handler stores all required context for handing off upload requests to a
@@ -24,7 +26,6 @@ func New(store objectstore.ObjectStore) *Handler {
 
 // ServeHTTP implements the http.Handler interface for serving responses.
 func (uh *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	contentType := r.Header.Get("Content-type")
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "Unable to read post body.", http.StatusInternalServerError)
@@ -39,6 +40,12 @@ func (uh *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		scheme = "http"
 	}
 
+	// lookup content-type of the data
+	contentType, err := getMimeString(body)
+	if err != nil {
+		http.Error(w, "Couldn't assertain mimetype from post body", http.StatusBadRequest)
+	}
+
 	obj := objectstore.New(contentType, body)
 	id := string(obj.Metadata.Object)
 
@@ -49,4 +56,17 @@ func (uh *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	fileURL := strings.Join([]string{scheme, "://", r.Host, r.RequestURI, id, "\n"}, "")
 	w.Write([]byte(fileURL))
+}
+
+func getMimeString(data []byte) (string, error) {
+	if err := magicmime.Open(magicmime.MAGIC_MIME_TYPE | magicmime.MAGIC_ERROR); err != nil {
+		log.Fatal(err)
+	}
+	defer magicmime.Close()
+
+	mimetype, err := magicmime.TypeByBuffer(data)
+	if err != nil {
+		log.Fatalf("error occured during type lookup: %v", err)
+	}
+	return mimetype, nil
 }
