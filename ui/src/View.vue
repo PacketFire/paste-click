6e9a45e662692d55b3bb4b035afad459a70c32b6
Raas Ahsan <agro@jantox.com>
2019-01-06 23:54:56 -0600

    add router-view

--- ui/src/View.vue
+++ ui/src/View.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-
+    View
   </div>
 </template>
 
--- ui/src/app.js
+++ ui/src/app.js
@@ -6,14 +6,22 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import App from './App.vue';
+import Up from './Up.vue';
+import View from './View.vue';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-
+  mode: 'history',
+  base: '/beta',
+  routes: [
+    { path: '/up', component: Up },
+    { path: '/s', component: View }
+  ]
 });
 
 new Vue({
   el: '#app',
+  router: router,
   render: h => h(App)
 });
