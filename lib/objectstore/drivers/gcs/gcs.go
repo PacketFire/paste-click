d81ba71f3807cfc69eeee190c15384fe5f2e4af7
Nate Catelli <ncatelli@packetfire.org>
2019-01-21 14:28:43 -0500

    Add write method to gcs driver

--- lib/objectstore/drivers/gcs/gcs.go
+++ lib/objectstore/drivers/gcs/gcs.go
@@ -1,26 +1,26 @@
 package gcs
 
 import (
-	"github.com/PacketFire/paste-click/lib/objectstore/metadata"
 	"bytes"
-	"io"
 	"errors"
+	"github.com/PacketFire/paste-click/lib/objectstore/metadata"
+	"io"
 
+	"cloud.google.com/go/storage"
+	"context"
 	"github.com/PacketFire/paste-click/lib/objectstore"
 	"github.com/PacketFire/paste-click/lib/objectstore/objectid"
 	"github.com/caarlos0/env"
-	"cloud.google.com/go/storage"
-	"context"
 )
 
 // Store implements the ObjectStore interface to store Objects in GCS
 type Store struct {
 	// the base path for reading and writing objects to.
-	BasePath  string `env:"STORE_FS_BASE_PATH,required"`
+	BasePath   string `env:"STORE_FS_BASE_PATH,required"`
 	BucketName string `env:"STORE_GCS_BUCKET_NAME,required"`
-	client *storage.Client
-	bucket *storage.BucketHandle
-	ctx context.Context
+	client     *storage.Client
+	bucket     *storage.BucketHandle
+	ctx        context.Context
 }
 
 // Init initializes the gcs store.
@@ -38,7 +38,7 @@ func (s *Store) Init() error {
 	s.client = client
 	s.bucket = client.Bucket(s.BucketName)
 
-	return nil 
+	return nil
 }
 
 // Read takes an ObjectID as an argument and attempts to read the corresponding
@@ -52,7 +52,7 @@ func (s *Store) Read(id objectid.ObjectID) (*objectstore.Object, error) {
 		return nil, err
 	}
 	defer r.Close()
-	
+
 	bodyBuf := new(bytes.Buffer)
 	if _, err := io.Copy(bodyBuf, r); err != nil {
 		return nil, err
@@ -64,10 +64,10 @@ func (s *Store) Read(id objectid.ObjectID) (*objectstore.Object, error) {
 	}
 
 	metadata := &metadata.Metadata{
-		Object: id,
+		Object:   id,
 		Mimetype: attrs.ContentType,
 		Uploaded: attrs.Created.String(),
-		Size: attrs.Size,
+		Size:     attrs.Size,
 	}
 
 	return &objectstore.Object{
@@ -80,6 +80,21 @@ func (s *Store) Read(id objectid.ObjectID) (*objectstore.Object, error) {
 // correctly written to the GCS bucket, nil is returned. Otherwise an error is
 // returned providing why the Object could not be correctly written.
 func (s *Store) Write(obj *objectstore.Object) error {
+	oid := string(obj.Metadata.Object)
+
+	w := s.bucket.Object(oid).NewWriter(s.ctx)
+	w.ACL = []storage.ACLRule{{Entity: storage.AllUsers, Role: storage.RoleReader}}
+	w.ContentType = obj.Metadata.Mimetype
+
+	buf := bytes.NewBuffer(obj.Data)
+
+	if _, err := io.Copy(w, buf); err != nil {
+		return err
+	}
+	if err := w.Close(); err != nil {
+		return err
+	}
+
 	return nil
 }
 
