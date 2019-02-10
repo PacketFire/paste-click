e8289351511404ee07d975de2bea93bd28930f0f
Nate Catelli <ncatelli@packetfire.org>
2019-02-10 15:45:31 -0600

    Enabling go checksums

--- lib/objectstore/object.go
+++ lib/objectstore/object.go
@@ -2,9 +2,11 @@ package objectstore
 
 import (
 	"crypto/md5"
+	"fmt"
 
 	"github.com/PacketFire/paste-click/lib/objectstore/metadata"
 	"github.com/PacketFire/paste-click/lib/objectstore/objectid"
+	hashids "github.com/speps/go-hashids"
 )
 
 // Object stores contains both the data and metadata for an object written to
@@ -18,7 +20,13 @@ type Object struct {
 func New(mimetype string, data []byte) *Object {
 	sum := md5.New()
 	sum.Write(data)
-	oid := objectid.New(sum)
+
+	hd := hashids.NewData()
+	hd.Salt = ""
+	h, _ := hashids.NewWithData(hd)
+	id, _ := h.EncodeHex(fmt.Sprintf("%x", sum.Sum(nil)))
+
+	oid := objectid.ObjectID(id)
 
 	md := metadata.New(int64(len(data)), mimetype, oid)
 	return &Object{
--- lib/objectstore/objectid/objectid.go
+++ lib/objectstore/objectid/objectid.go
@@ -1,17 +1,4 @@
 package objectid
 
-import (
-	"fmt"
-	"hash"
-)
-
 // ObjectID contains the string representation of an objects identifier.
 type ObjectID string
-
-// New takes a Hash as an argument and uses that to generate a new ObjectID.
-func New(h hash.Hash) ObjectID {
-	// Generate checksum and truncate it to 6 characters
-	checksum := fmt.Sprintf("%x", h.Sum(nil))
-
-	return ObjectID(checksum)
-}
