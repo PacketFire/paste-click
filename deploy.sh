b90f16adaa30a2548fd016a6977d3919b2fa449c
Raas Ahsan <agro@jantox.com>
2019-02-13 20:32:19 -0600

    set correct container

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ kubectl --server="$GKE_APISERVER" \
         --namespace="$GKE_NAMESPACE" \
         --token="$GKE_BEARER_TOKEN" \
         --insecure-skip-tls-verify=true \
-        set image deployment/paste-click nginx=gcr.io/$GCP_PROJECT_ID/paste-click:latest
+        set image deployment/paste-click paste-click=gcr.io/$GCP_PROJECT_ID/paste-click:latest
         
 kubectl --server="$GKE_APISERVER" \
         --namespace="$GKE_NAMESPACE" \
         --token="$GKE_BEARER_TOKEN" \
         --insecure-skip-tls-verify=true \
-        set image deployment/paste-click nginx=gcr.io/$GCP_PROJECT_ID/paste-click
+        set image deployment/paste-click paste-click=gcr.io/$GCP_PROJECT_ID/paste-click
 
 kubectl --server="$GKE_APISERVER" \
         --namespace="$GKE_NAMESPACE" \
