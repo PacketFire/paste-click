f1c1c0ee412791246290c56d0f49d2795105f78f
Raas Ahsan <agro@jantox.com>
2019-02-13 21:14:20 -0600

    Deploy images with commit sha

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,10 @@ kubectl --server="$GKE_APISERVER" \
         --namespace="$GKE_NAMESPACE" \
         --token="$GKE_BEARER_TOKEN" \
         --insecure-skip-tls-verify=true \
-        set image deployment/paste-click paste-click=gcr.io/$GCP_PROJECT_ID/paste-click:latest
+        set image deployment/paste-click paste-click=gcr.io/$GCP_PROJECT_ID/paste-click:$TRAVIS_COMMIT
         
 kubectl --server="$GKE_APISERVER" \
         --namespace="$GKE_NAMESPACE" \
         --token="$GKE_BEARER_TOKEN" \
         --insecure-skip-tls-verify=true \
-        set image deployment/paste-click paste-click=gcr.io/$GCP_PROJECT_ID/paste-click
-
-kubectl --server="$GKE_APISERVER" \
-        --namespace="$GKE_NAMESPACE" \
-        --token="$GKE_BEARER_TOKEN" \
-        --insecure-skip-tls-verify=true \
-        set image deployment/nginx nginx=gcr.io/$GCP_PROJECT_ID/openresty-paste-click:latest
-        
-kubectl --server="$GKE_APISERVER" \
-        --namespace="$GKE_NAMESPACE" \
-        --token="$GKE_BEARER_TOKEN" \
-        --insecure-skip-tls-verify=true \
-        set image deployment/nginx nginx=gcr.io/$GCP_PROJECT_ID/openresty-paste-click
+        set image deployment/nginx nginx=gcr.io/$GCP_PROJECT_ID/openresty-paste-click:$TRAVIS_COMMIT
