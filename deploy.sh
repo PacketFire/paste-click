d5f0646918cb9cce10eafe9464ea757790326122
Raas Ahsan <agro@jantox.com>
2019-02-13 20:22:55 -0600

    specify gke namespace

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 kubectl --server="$GKE_APISERVER" \
+        --namespace="$GKE_NAMESPACE" \
         --token="$GKE_BEARER_TOKEN" \
         --insecure-skip-tls-verify=true \
         set image deployment/paste-click nginx=gcr.io/$GCP_PROJECT_ID/paste-click:latest
         
 kubectl --server="$GKE_APISERVER" \
+        --namespace="$GKE_NAMESPACE" \
         --token="$GKE_BEARER_TOKEN" \
         --insecure-skip-tls-verify=true \
         set image deployment/paste-click nginx=gcr.io/$GCP_PROJECT_ID/paste-click
 
 kubectl --server="$GKE_APISERVER" \
+        --namespace="$GKE_NAMESPACE" \
         --token="$GKE_BEARER_TOKEN" \
         --insecure-skip-tls-verify=true \
         set image deployment/nginx nginx=gcr.io/$GCP_PROJECT_ID/openresty-paste-click:latest
         
 kubectl --server="$GKE_APISERVER" \
+        --namespace="$GKE_NAMESPACE" \
         --token="$GKE_BEARER_TOKEN" \
         --insecure-skip-tls-verify=true \
         set image deployment/nginx nginx=gcr.io/$GCP_PROJECT_ID/openresty-paste-click
