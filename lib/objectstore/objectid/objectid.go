596092a9cb148fdb0b37eb90fe8c5a4167736e75
Nate Catelli <ncatelli@packetfire.org>
2019-02-20 17:27:24 -0600

    Switch base62 to URL-safe base64

--- lib/objectstore/objectid/objectid.go
+++ lib/objectstore/objectid/objectid.go
@@ -1,9 +1,8 @@
 package objectid
 
 import (
+	"encoding/base64"
 	"hash"
-
-	"github.com/lytics/base62"
 )
 
 // ObjectID contains the string representation of an objects identifier.
@@ -12,7 +11,5 @@ type ObjectID string
 // New takes a Hash as an argument and uses that to generate a new ObjectID.
 func New(h hash.Hash) ObjectID {
 	// Generate checksum and truncate it to 6 characters
-	//checksum := fmt.Sprintf("%x", h.Sum(nil))
-
-	return ObjectID(base62.StdEncoding.EncodeToString(h.Sum(nil)))
+	return ObjectID(base64.URLEncoding.EncodeToString(h.Sum(nil)))
 }
