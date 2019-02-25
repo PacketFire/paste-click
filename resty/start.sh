a5e30f414afc906026466d89bc77b485c42a3d86
Raas Ahsan <agro@jantox.com>
2019-02-25 00:55:34 -0600

    fix sed command

--- resty/start.sh
+++ resty/start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-sed "s:__PASTE_CLICK_UPSTREAM__:$PASTE_CLICK_UPSTREAM:g; s:__PASTE_CLICK_FRONTEND_BUCKET__:$PASTE_CLICK_FRONTEND_BUCKET:g;" \
-    < /usr/local/openresty/nginx/conf/sites-enabled/paste.click.conf \
-    > /usr/local/openresty/nginx/conf/sites-enabled/paste.click.conf
+sed -i "s|__PASTE_CLICK_UPSTREAM__|$PASTE_CLICK_UPSTREAM|g;s|__PASTE_CLICK_FRONTEND_BUCKET__|$PASTE_CLICK_FRONTEND_BUCKET|g" \
+    /usr/local/openresty/nginx/conf/sites-enabled/paste.click.conf
 
 /usr/local/openresty/bin/openresty -g "daemon off;"
