310ec680434ab2f33cdf0c5eede34f33e72ee398
Raas Ahsan <agro@jantox.com>
2019-02-25 00:00:10 -0500

    Gcs proxy (#51)
    * Mount configuration into image
    
    * sed in docker image
    
    * start script
    
    * remove travis sed
    
    * global sde
    
    * gcs proy
    
    * nginx configuration
    
    * set env

--- resty/start.sh
+++ resty/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed "s;__PASTE_CLICK_UPSTREAM__;$PASTE_CLICK_UPSTREAM;g" \
+sed "s:__PASTE_CLICK_UPSTREAM__:$PASTE_CLICK_UPSTREAM:g; s:__PASTE_CLICK_FRONTEND_BUCKET__:$PASTE_CLICK_FRONTEND_BUCKET:g;" \
     < /usr/local/openresty/nginx/conf/sites-enabled/paste.click.conf \
     > /usr/local/openresty/nginx/conf/sites-enabled/paste.click.conf
 
